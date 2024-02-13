144863a08af09ecbad514dd388e495831e0f6b3a
elmonofy <104175354+elmonofy@users.noreply.github.com>
2024-02-13 04:32:40 +0200

    فشسن

---
+++ S7/script.js
@@ -0,0 +1,120 @@
+// var FirstName = prompt("Enter The First Name")
+// var LastName = Prompt("Enter The Last NAme")
+
+// if(FirstName ==="Kaled" && LastName==="Magdy"){
+//     console.log("hello" + FirstName + LastName)
+// }else{
+//     console.log("sorry")
+// }
+
+// var Word = prompt("Enter The Word")
+
+// a=
+
+// if (Word[0] ==)
+
+
+
+
+
+
+
+
+
+
+
+// assiment 2
+
+// var x= prompt("enter the number")
+
+// if ( x % 3 && x % 4 ){
+//     console.log("Yeees")
+// }else{
+//     console.log("No")
+// }
+
+
+
+
+
+
+// assiment 3
+
+
+// var firstNumber = prompt("enter first number")
+// var secondNumber = prompt("enter second number")
+
+// if (firstNumber < secondNumber){
+//     console.log("the max number = " + firstNumber)
+// }
+// else if (firstNumber > secondNumber){
+//     console.log("the max number = " + secondNumber)
+// }
+// else{
+//     console.log("the two number is equalized")
+// }
+
+
+
+
+
+// assiment 4
+
+
+// var x  = prompt("enter the number")
+
+// if (x > 0){
+//     console.log("the number is positive")
+// }
+// else if (x  < 0){
+//     console.log("the number is negative")
+// }
+// else{
+//     console.log("the number is zero")
+// }
+
+
+
+
+// assiment 5
+
+// var first  = prompt("enter the first number")
+// var second  = prompt("enter the second number")
+// var third  = prompt("enter the third number")
+
+// if (first> second && first>third){
+//     console.log("the max number is " + first)
+// }
+// else if (second > first && second > third ){
+//     console.log("the max number is " + second)  
+// }
+// else{
+//     console.log("the max number is " + third)
+// }
+
+
+
+
+
+//assiment 6
+
+// var x = prompt("enter the number")
+
+// for (i=0 ; i<=x ; i++){
+//     console.log(i)
+// }
+
+
+
+
+//assiment 7
+
+// var x = prompt("enter the number")
+// for (i=1 ; i<=12 ; i++){
+//     console.log(x*i)
+// }
+
+
+
+//assiment 8
+
---
+++ S7/script2.js
@@ -0,0 +1,141 @@
+// var x =prompt("enter number") 
+// for( i = 1 ; i<=x ; i++ ){
+//     console.log(i)
+// }
+
+
+
+
+// var x = prompt("enter the number")
+// for (i=1 ; i<=12 ; i++){
+//     console.log(x*i)
+// }
+
+
+
+
+
+// var x = prompt("enter the number")
+// for (i=1 ; i<=x ; i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+//     else{
+//     }
+// }
+
+
+
+// var x = prompt("enter the number")
+// var y = prompt("enter the power")
+// var result
+// for (i = 0 ; i<=y ; i++){
+//         if(i==0){
+//             result = 0
+//         }else if(i==1){
+//             result = x
+//         }else if(i==2){
+//             result = x*x 
+//         }else{
+//             result *=x
+//         }
+//     }
+// console.log("the result is equal " + result )    
+
+
+
+
+// var s1 = prompt("enter the first sub")
+// var s2 = prompt("enter the second sub")
+// var s3 = prompt("enter the third sub")
+// var s4 = prompt("enter the fourth sub")
+// var s5 = prompt("enter the fifth sub")
+
+// var total =  parseInt(s1)+ parseInt(s2)+ parseInt(s3)+ parseInt(s4)+ parseInt(s5)
+// var average = total /5
+// var percentage = total *100 /500
+
+// console.log("Total marks = "+total);
+// console.log("Average Marks "+average);
+// console.log("Percentage = "+percentage);
+
+
+
+// var x = prompt("enter the month number")
+
+// if (x<=12 && x%2==0){
+//     if(x==2){
+//         console.log("the month is fub");
+//     }else if(x==4){
+//         console.log("the month is apr");
+//     }else if(x==6){
+//         console.log("the month is june");
+//     }else if(x==8){
+//         console.log('the month is aug');
+//     }else if(x==10){
+//         console.log("the month is oct");
+//     }else if(x==12){
+//         console.log('the month is dec');
+//     }
+// }else{
+//     if(x==1){
+//         console.log("the month is jun");
+//     }else if(x==3){
+//         console.log("the month is mar");
+//     }else if(x==5){
+//         console.log('the month is may');
+//     }else if(x==7){
+//         console.log('the month is july');
+//     }else if(x==9){
+//         console.log("the month is seb");
+//     }else if(x==11){
+//         console.log("the month is nov");
+//     }
+// }
+
+
+
+
+
+
+
+
+
+// var x = prompt("enter the month number")
+
+// if (x<=12 && x%2==0){
+//         console.log('the month is 31 days');
+// }else if (x<=12 && x%2!=0){
+//         console.log("the month is 30 days");
+// }else{
+//     console.log("not a right month");
+// }
+
+
+
+
+
+
+
+
+// var s1 = prompt("enter the Physics")
+// var s2 = prompt("enter the Chemistry")
+// var s3 = prompt("enter the Biology")
+// var s4 = prompt("enter the Mathematics")
+// var s5 = prompt("enter the Computer")
+
+// var total =  parseInt(s1)+ parseInt(s2)+ parseInt(s3)+ parseInt(s4)+ parseInt(s5)
+// var percentage = total *100 /500
+// if(100>=percentage>=90){
+//     console.log("Grad A");
+// }else if(90>percentage>=80){
+//     console.log("Grad B");
+// }else if(80>percentage>=70){
+//     console.log("Grad C");
+// }else if(70>percentage>=60){
+//     console.log("Grad D");
+// }else if(60>percentage>=40){
+//     console.log("Grad E");
+// }else{
+//     console.log("Grad F");
+// }
